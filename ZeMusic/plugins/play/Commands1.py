221b7cec20fd53f4287d2ce54477ae94c3652739
Newwissam <mmdhkwr85@gmail.com>
2024-06-06 15:19:11 +0300

    Update Commands1.py

--- ZeMusic/plugins/play/Commands1.py
+++ ZeMusic/plugins/play/Commands1.py
@@ -1,38 +1,35 @@
-import asyncio
+import os
+import random
+import requests
+from datetime import datetime
+from sys import version_info
+from time import time
 from pyrogram import Client, filters
-from strings import get_command
-from strings.filters import command
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
-from YukkiMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
-from pyrogram import filters
-from pyrogram.types import (InlineKeyboardButton,CallbackQuery,
-                            InlineKeyboardMarkup, Message)
-from youtubesearchpython.__future__ import VideosSearch
-from typing import Union
-
-from pyrogram.types import InlineKeyboardButton
-
+from ZeMusic import app
+from ZeMusic import Mody
+from ZeMusic.utils.decorators.admins import AdminActual
 from config import GITHUB_REPO, SUPPORT_CHANNEL, SUPPORT_GROUP
 from YukkiMusic import app
 import config
 from config import BANNED_USERS
 from config.config import OWNER_ID
 from strings import get_command, get_string
-from YukkiMusic import Telegram, YouTube, app
-from YukkiMusic.misc import SUDOERS
-from YukkiMusic.plugins.play.playlist import del_plist_msg
-from YukkiMusic.plugins.sudo.sudoers import sudoers_list
-from YukkiMusic.utils.database import (add_served_chat,
+from ZeMusic import Telegram, YouTube, app
+from ZeMusic.misc import SUDOERS
+from ZeMusic.plugins.play.playlist import del_plist_msg
+from ZeMusic.plugins.sudo.sudoers import sudoers_list
+from ZeMusic.utils.database import (add_served_chat,
                                        add_served_user,
                                        blacklisted_chats,
                                        get_assistant, get_lang,
                                        get_userss, is_on_off,
                                        is_served_private_chat)
-from YukkiMusic.utils.decorators.language import LanguageStart
-from YukkiMusic.utils.inline import (help_pannel, private_panel,
+from ZeMusic.utils.decorators.language import LanguageStart
+from ZeMusic.utils.inline import (help_pannel, private_panel,
                                      start_pannel)
 
-from YukkiMusic import check_client
+from ZeMusic import check_client
 
 
 @app.on_callback_query(filters.regex("ddd"))
