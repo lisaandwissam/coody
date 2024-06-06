d2d438cc4876eeac41d15a2bf19bd18f857070f8
Newwissam <mmdhkwr85@gmail.com>
2024-06-06 15:23:40 +0300

    Update Commands1.py

--- ZeMusic/plugins/play/Commands1.py
+++ ZeMusic/plugins/play/Commands1.py
@@ -12,7 +12,6 @@ from ZeMusic.utils.decorators.admins import AdminActual
 from ZeMusic import app
 import config
 from config import BANNED_USERS
-from config.config import OWNER_ID
 from strings import get_command, get_string
 from ZeMusic import Telegram, YouTube, app
 from ZeMusic.misc import SUDOERS
